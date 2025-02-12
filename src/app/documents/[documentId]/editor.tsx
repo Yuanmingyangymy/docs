a4e49e80ee20d408656dc6fe704ab1d3665524dc
Yuanmingyangymy <2329435441@qq.com>
2025-02-12 16:26:11 +0800

    使用zustand管理editor状态；添加工具栏列表

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -10,11 +10,40 @@ import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
 import Image from "@tiptap/extension-image";
 import ImageResize from "tiptap-extension-resize-image";
+import Underline from "@tiptap/extension-underline";
+import { useEditorStore } from "@/store/use-editor-store";
 
 function Editor() {
+  const { setEditor } = useEditorStore();
   const editor = useEditor({
+    // 针对各种情形更新editor全局状态
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
     extensions: [
       StarterKit,
+      Underline,
       TaskList,
       TaskItem.configure({
         nested: true,
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Editor from "./editor";
+import Toolbar from "./toolbar";
 
 interface DocumentIdParams {
   params: Promise<{ documentId: string }>;
@@ -8,6 +9,7 @@ async function DocumentIdPage({ params }: DocumentIdParams) {
   const { documentId } = await params;
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
       <Editor />
     </div>
   );
